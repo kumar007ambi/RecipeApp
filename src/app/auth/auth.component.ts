64d5f7ac89076ae9e62e1ef422bce83c57cbad0b
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-09-18 18:37:19 +0530

    some changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -19,29 +19,38 @@ export class AuthComponent {
   }
 
   onSubmit(form: NgForm) {
-    if (!form.valid) return;
+    if (!form.valid) {
+      return;
+    }
     const email = form.value.email;
     const password = form.value.password;
     this.isLoading = true;
 
-    let authObs: Observable<AuthResponceData>;
+    // let authObs: Observable<AuthResponceData>;
 
     if (this.isLoginMode) {
-      authObs = this.authService.login(email, password);
+    //  authObs = this.authService.login(email, password);
+    this.isLoading = false;
     } else {
-      authObs = this.authService.signup(email, password);
+      //authObs = this.authService.signup(email, password);
+      this.authService.signup(email,password).subscribe(resData=>{
+        console.log(resData)
+      },error=>{
+        console.log(error)
+      })
+      this.isLoading = false;
     }
-    authObs.subscribe(
-      (resData) => {
-        console.log(resData);
-        this.isLoading = false;
-      },
-      (errorMessage) => {
-        console.log(errorMessage);
-        this.error = errorMessage;
-        this.isLoading = false;
-      }
-    );
+    // authObs.subscribe(
+    //   (resData) => {
+    //     console.log(resData);
+    //     this.isLoading = false;
+    //   },
+    //   (errorMessage) => {
+    //     console.log(errorMessage);
+    //     this.error = errorMessage;
+    //     this.isLoading = false;
+    //   }
+    // );
     form.reset();
   }
 }
--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'spinner-app',
-  template: '<div class="lds-dual-ring"></div>',
+  template: '<div class="lds-spinner"><div></div><div></div><div></div><div>',
   styleUrls: ['./spinner.component.css'],
 })
 export class SpinnerComponent {}
