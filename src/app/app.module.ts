fc852a0aa0634b55e25d75ba84ed1fecc9476638
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-09-29 21:58:32 +0530

    added token  interceptor srevice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +19,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 
 @NgModule({
@@ -44,7 +45,8 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [ShopingListService,RecipeService],
+  providers: [ShopingListService, RecipeService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -24,25 +24,22 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(take(1), exhaustMap(user => {
-      return this.http.get<Recipe[]>(
-        'https://ng-course-recipe-bf965-default-rtdb.firebaseio.com/recipes.json',
-        {
-          params: new HttpParams().set('auth', user.token)
-        }
-      )
-    }),
-      map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : []
-          }
+
+    return this.http.get<Recipe[]>(
+      'https://ng-course-recipe-bf965-default-rtdb.firebaseio.com/recipes.json',
+    )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            }
+          })
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes)
         })
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes)
-      })
-    );
+      );
   }
 }
